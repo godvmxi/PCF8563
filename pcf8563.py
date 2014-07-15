1f935535bd280a56b57512dbf456ad440d53c4e8
root <root@raspberrypi.(none)>
2014-07-15 20:47:57 +0000

    fix for invalid datetime values

--- pcf8563.py
+++ pcf8563.py
@@ -27,7 +27,12 @@ if __name__ == "__main__":
 	h = ((clk[2] & 0x30) / 16) * 10 + (clk[2] & 0x0F)
 	m = ((clk[1] & 0x70) / 16) * 10 + (clk[1] & 0x0F)
 	s = ((clk[0] & 0x70) / 16) * 10 + (clk[0] & 0x0F)
-	pcf = datetime.datetime(Y, M, D, h, m, s, 0 , None)
+	
+	try:
+		pcf = datetime.datetime(Y, M, D, h, m, s, 0 , None)
+	except:
+		pcf = None
+		print "Error converting to datetime [%s, %s, %s, %s, %s, %s]!" % (Y, M, D, h, m, s)
 	
 	if len(sys.argv)>1:
 		if sys.argv[1] == 'ntp':
@@ -46,8 +51,13 @@ if __name__ == "__main__":
 			s = (dt.second / 10) * 16 + dt.second % 10
 			pcf8563.write_i2c_block_data(DEVICE_ADDRESS, 0x02, [s, m, h, D, WD, M, Y])
 		elif sys.argv[1] == 'print':
-			print pcf.strftime('%m%d%H%M%Y.%S')
+			if pcf:
+				print pcf.strftime('%m%d%H%M%Y.%S')
 		else:
-			print pcf
+			if pcf:
+				print pcf
 	else:
-		print pcf.strftime('%a %b %d %H:%M:%S UTC %Y')
+		if pcf:
+			print pcf.strftime('%a %b %d %H:%M:%S UTC %Y')
+		else:
+			print clk
